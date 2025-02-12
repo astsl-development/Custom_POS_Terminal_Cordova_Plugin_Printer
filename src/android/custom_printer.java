4cf67037f7d4ee64c68cc4af69c5196122a6f0f6
astsl-development <150181793+astsl-development@users.noreply.github.com>
2025-02-12 14:56:51 -0400

    Add files via upload

--- src/android/custom_printer.java
+++ src/android/custom_printer.java
@@ -1,22 +1,39 @@
 package custom_printer;
 
+import android.os.Handler;
+
 import org.apache.cordova.CordovaPlugin;
+
+import java.rmi.RemoteException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import net.nyx.printerservice.print.PrintTextFormat;
+import net.nyx.printerservice.print.IPrinterService;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class custom_printer extends CordovaPlugin {
 
+
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
             String message = args.getString(0);
+        if (action.equals("coolMethod")) {
+            message = args.getString(0);
             this.coolMethod(message, callbackContext);
+            return true;
+        }else if(action.equals("printBill")){
+            message = args.getString(0);
+            this.setPrinter(message,callbackContext);
             return true;
         }
         return false;
@@ -29,4 +46,91 @@ public class custom_printer extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {            
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void printBill(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            printText(message);
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+    
+    private IPrinterService printerService;
+    printerService = IPrinterService.Stub.asInterface(service);
+    private ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+
+    private void printText(String text) {
+        singleThreadExecutor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    PrintTextFormat textFormat = new PrintTextFormat();
+                    // textFormat.setTextSize(32);
+                    // textFormat.setUnderline(true);
+                    int ret = printerService.printText(text, textFormat);                    
+                    if (ret == 0) {
+                        printerService.printEndAutoOut();
+                    }
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    // private void setPrinter(String message, CallbackContext callbackContext) {
+    //     if (message != null && message.length() > 0) {
+
+    //         callbackContext.success(message);
+    //     } else {
+    //         callbackContext.error("Expected one non-empty string argument.");
+    //     }
+    // }
 }
+
+
+// private Handler handler = new Handler();
+
+
+// private ServiceConnection connService = new ServiceConnection() {
+//     @Override
+//     public void onServiceDisconnected(ComponentName name) {            
+//         printerService = null;
+//         handler.postDelayed(() -> bindService(), 5000);
+//     }
+
+//     @Override
+//     public void onServiceConnected(ComponentName name, IBinder service) {            
+//         printerService = IPrinterService.Stub.asInterface(service);            
+//     }
+// };
+
+// private void bindService() {
+//     Intent intent = new Intent();
+//     intent.setPackage("net.nyx.printerservice");
+//     intent.setAction("net.nyx.printerservice.IPrinterService");
+//     bindService(intent, connService, Context.BIND_AUTO_CREATE);
+// }
+
+// private void unbindService() {
+//     unbindService(connService);
+// }
--- www/custom_printer.js
+++ www/custom_printer.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'custom_printer', 'coolMethod', [arg0]);
 };
+
+exports.printBill = function (arg0, success, error) {
+    exec(success, error, 'custom_printer', 'printBill', [arg0]);
+};
